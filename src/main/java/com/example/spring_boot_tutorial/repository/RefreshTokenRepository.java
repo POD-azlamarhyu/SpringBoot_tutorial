a30461240296b6b9e2c7719a5ac6a1c840a96602
azla mahryu <sgommccisbad72145@gmail.com>
2024-10-13 03:52:53 +0900

    edit anotation at refreshtoken repo

--- src/main/java/com/example/spring_boot_tutorial/repository/RefreshTokenRepository.java
+++ src/main/java/com/example/spring_boot_tutorial/repository/RefreshTokenRepository.java
@@ -1,10 +1,12 @@
 package com.example.spring_boot_tutorial.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.spring_boot_tutorial.entity.RefreshToken;
 import com.example.spring_boot_tutorial.entity.User;
 
+@Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long>{
     RefreshToken findByToken(String token);
 
