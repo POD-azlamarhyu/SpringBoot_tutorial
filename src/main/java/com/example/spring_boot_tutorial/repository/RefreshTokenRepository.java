f888b7bbcccc39d0675aa6195fa7afa527afd8fc
azla mahryu <sgommccisbad72145@gmail.com>
2024-10-14 14:08:29 +0900

    unused class remove

--- src/main/java/com/example/spring_boot_tutorial/repository/RefreshTokenRepository.java
+++ src/main/java/com/example/spring_boot_tutorial/repository/RefreshTokenRepository.java
@@ -2,7 +2,6 @@ package com.example.spring_boot_tutorial.repository;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
