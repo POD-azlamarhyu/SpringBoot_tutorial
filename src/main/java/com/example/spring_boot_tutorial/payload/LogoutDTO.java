bd5795fa12768ab31650b25eba006b8e60e13774
azla mahryu <sgommccisbad72145@gmail.com>
2024-10-15 22:15:33 +0900

    add logout dto

---
+++ src/main/java/com/example/spring_boot_tutorial/payload/LogoutDTO.java
@@ -0,0 +1,14 @@
+package com.example.spring_boot_tutorial.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LogoutDTO {
+    private String refreshToken;
+}
